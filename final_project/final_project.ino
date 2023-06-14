1ea683db6489ad1be06cb2cdb4e13f4d475b5deb
Jamie Liu <44714393+jmliu1417@users.noreply.github.com>
2023-06-14 10:32:02 -0700

    Update final_project.ino
    Finished up the bugs, added more correct outputs, and changed the colors slightly. Should actually kind of work now!

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -43,7 +43,19 @@ int offSong[4][2] = {
   { 58, 100 },
 };
 
+int victory[5][2] = {
+  { 60, 70 },
+  { 60, 70 },
+  { 60, 70 },
+  { 68, 250 },
+  {200, 250},
+};
+
 int buttonPressed = 0;
+
+bool bool1 = false;
+bool bool2 = false;
+bool bool3 = false;
 
 
 void setup() {
@@ -72,26 +84,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(switchFlag){
+  if (switchFlag) {
     delay(5);
     switchVal = digitalRead(switchPin);
     switchFlag = 0;
   }
   // startup code
-  if(switchVal){
-
-    bool level1Return = false;
+  if (switchVal) {
 
     for (int i = 0; i < sizeof(startSong) / sizeof(startSong[0]); i++) {
       CircuitPlayground.playTone(midi[startSong[i][0]], startSong[i][1]);
-      }
+    }
 
-      for(int i=0; i<3; i++){
-      for(int j=0; j<10; j++){
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 10; j++) {
         CircuitPlayground.setPixelColor(j, 0, 255, 0);
       }
       delay(300);
-      for(int j=0; j<10; j++){
+      for (int j = 0; j < 10; j++) {
         CircuitPlayground.setPixelColor(j, 0, 0, 0);
       }
       delay(300);
@@ -99,35 +109,43 @@ void loop() {
 
 
     level1();
-    if(level1() == true){
-      Serial.println("okay");
-    }else{
-      Serial.println("uhhh");
-    }
-//this is the initial startup code, could probably fit in a function.
-    
-  }else if(!switchVal){
-    delay(5);
-
-    for (int i = 0; i < sizeof(offSong) / sizeof(offSong[0]); i++) {
-      CircuitPlayground.playTone(midi[offSong[i][0]], offSong[i][1]);
+    if (bool1 == true) {
+      level2();
+      if (bool2 == true) {
+        level3();
+        if (bool3 == true) {
+          for (int i = 0; i < sizeof(victory) / sizeof(victory[0]); i++) {
+            CircuitPlayground.playTone(midi[victory[i][0]], victory[i][1]);
+          }
+        }
       }
-    
-    //turning off sequence (lights)
-    for(int i=0; i<3; i++){
-      for(int i=0; i<10; i++){
-        CircuitPlayground.setPixelColor(i, 255, 0, 0);
-      }
-      delay(300);
-      for(int i=0; i<10; i++){
-        CircuitPlayground.setPixelColor(i, 0, 0, 0);
-      }
-      delay(300);
     }
 
-    
+    bool1 = false;
+    bool2 = false;
+    bool3 = false;
+
+    //this is the initial startup code, could probably fit in a function.
+    //switchVal = 0;
+  } else if (!switchVal) {
+      delay(5);
+
+      for (int i = 0; i < sizeof(offSong) / sizeof(offSong[0]); i++) {
+        CircuitPlayground.playTone(midi[offSong[i][0]], offSong[i][1]);
+      }
+
+      //turning off sequence (lights)
+      for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
+          CircuitPlayground.setPixelColor(i, 255, 0, 0);
+        }
+        delay(300);
+        for (int i = 0; i < 10; i++) {
+          CircuitPlayground.setPixelColor(i, 0, 0, 0);
+        }
+        delay(300);
+      }
   }
-
 }
 
 void onOff7() {
@@ -149,42 +167,217 @@ void generateMIDI() {
   }
 }
 
-bool level1(){
+bool level1() {
   int randomLED = random(10);
 
-  for(int i=0; i<10; i++){
-    CircuitPlayground.setPixelColor(i, 254, 150, 250);
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 123, 231);
   }
 
   CircuitPlayground.setPixelColor(randomLED, 255, 230, 0);
   delay(1000);
 
-  for(int i=0; i<10; i++){
+  for (int i = 0; i < 10; i++) {
     CircuitPlayground.setPixelColor(i, 255, 0, 0);
     delay(1000);
 
     Serial.println(i);
 
-    if(rightFlag || leftFlag){
+    if (rightFlag || leftFlag) {
       buttonPressed = i;
-      if(buttonPressed == randomLED){
+      if (buttonPressed == randomLED) {
 
         for (int i = 0; i < sizeof(happySong) / sizeof(happySong[0]); i++) {
-      CircuitPlayground.playTone(midi[happySong[i][0]], happySong[i][1]);
-      }
+          CircuitPlayground.playTone(midi[happySong[i][0]], happySong[i][1]);
+        }
 
-        return true;
-      }else{
+        rightFlag = 0;
+        leftFlag = 0;
+        bool1 = true;
+        return bool1;
+
+      } else {
 
         for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
-      CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
-      }
-        return false;
-        break;
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+
+        rightFlag = 0;
+        leftFlag = 0;
+        bool1 = false;
+        return bool1;
       }
     }
+      
+  }
+  for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
 
-    
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+  return bool1;
+}
+
+
+bool level2() {
+  int randomLED = random(10);
+
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 40, 221);
   }
 
+  CircuitPlayground.setPixelColor(randomLED, 250, 230, 0);
+  delay(1000);
+
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 0, 0);
+    delay(500);
+
+    Serial.println(i);
+
+    if (rightFlag || leftFlag) {
+      buttonPressed = i;
+      if (buttonPressed == randomLED) {
+
+        for (int i = 0; i < sizeof(happySong) / sizeof(happySong[0]); i++) {
+          CircuitPlayground.playTone(midi[happySong[i][0]], happySong[i][1]);
+        }
+
+        rightFlag = 0;
+        leftFlag = 0;
+
+        bool2 = true;
+        return bool2;
+      } else {
+
+        for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+
+        rightFlag = 0;
+        leftFlag = 0;
+
+        bool2 = false;
+        return bool2;
+      }
+    }
+  }
+  for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+    return bool2;
+  }
+
+
+bool level3() {
+  int randomLED = random(10);
+
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 0, 204);
+  }
+
+  CircuitPlayground.setPixelColor(randomLED, 250, 230, 0);
+  delay(1000);
+
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 0, 0);
+    delay(100);
+
+    Serial.println(i);
+
+    if (rightFlag || leftFlag) {
+      buttonPressed = i;
+      if (buttonPressed == randomLED) {
+
+        for (int i = 0; i < sizeof(happySong) / sizeof(happySong[0]); i++) {
+          CircuitPlayground.playTone(midi[happySong[i][0]], happySong[i][1]);
+        }
+        rightFlag = 0;
+        leftFlag = 0;
+
+        bool3 = true;
+        return bool3;
+      } else {
+
+        for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+
+        rightFlag = 0;
+        leftFlag = 0;
+
+        bool3 = false;
+        return bool3;
+      }
+    }
+  }
+  for (int i = 0; i < sizeof(sadSong) / sizeof(sadSong[0]); i++) {
+          CircuitPlayground.playTone(midi[sadSong[i][0]], sadSong[i][1]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 255, 0, 0);
+          }
+          delay(300);
+          for (int j = 0; j < 10; j++) {
+            CircuitPlayground.setPixelColor(j, 0, 0, 0);
+          }
+          delay(300);
+        }
+  return bool3;
 }
+
